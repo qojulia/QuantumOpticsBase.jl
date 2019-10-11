4da570a45f83fc9a8e181770b4863c93cffdc5b7
Colm Ryan <colm@colmryan.org>
2019-10-10 22:08:06 -0700

    :racehorse: faster and more concise check for unique embedding indices

--- src/sortedindices.jl
+++ src/sortedindices.jl
@@ -110,16 +110,8 @@ function check_embed_indices(indices::Array)
     err_str = "Variable `indices` comes in an unexpected form. Expecting `Array{Union{Int, Array{Int, 1}}, 1}`"
     @assert mapreduce(x -> typeof(x)<:Array || typeof(x)<:Int, &, indices) err_str
 
-    isunique = true
-    # Check that no sub-list contains duplicates.
-    for i in filter(x -> typeof(x) <: Array, indices)
-        isunique &= (length(Set(i)) == length(i))
-    end
-    # Check that there are no duplicates across `indices`
-    for (idx, i) in enumerate(indices[1:end-1])
-        for j in indices[idx+1:end]
-            isunique &= (length(Base.intersect(i, j)) == 0)
-        end
-    end
-    return isunique
+    # flatten the indices and check for uniqueness
+    # use a custom flatten because it's ≈ 4x  faster than Base.Iterators.flatten
+    flatten(arr::Vector) = mapreduce(x -> x isa Vector ? flatten(x) : x, append!, arr, init=[])
+    allunique(flatten(indices))
 end
