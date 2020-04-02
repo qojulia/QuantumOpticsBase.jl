0e39856ba0346d3c7096c7130744257122f1f5b7
David Plankensteiner <david.plankensteiner@gmx.at>
2020-04-02 12:33:02 +0200

    Specialized dagger for sparse operators

--- src/operators_sparse.jl
+++ src/operators_sparse.jl
@@ -19,6 +19,10 @@ SparseOperator(op::DataOperator) = SparseOperator(op.basis_l, op.basis_r, op.dat
 SparseOperator(b1::Basis, b2::Basis) = SparseOperator(b1, b2, spzeros(ComplexF64, length(b1), length(b2)))
 SparseOperator(b::Basis) = SparseOperator(b, b)
 
+# Specialized dagger and transpose for sparse operators
+dagger(op::SparseOpType) = SparseOperator(op.basis_l, op.basis_r, convert(SparseMatrixCSC{ComplexF64,Int}, op.data'))
+transpose(op::SparseOpType) = SparseOperator(op.basis_l, op.basis_r, convert(SparseMatrixCSC{ComplexF64,Int}, transpose(op.data)))
+
 """
     sparse(op::AbstractOperator)
 
