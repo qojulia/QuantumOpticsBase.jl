6fe48e8656802e4a60eb7e59ae24e10abd222bf9
David Plankensteiner <david-pl@users.noreply.github.com>
2020-08-19 15:42:07 +0200

    Fix dispatch in embed docstring

--- src/operators.jl
+++ src/operators.jl
@@ -114,7 +114,7 @@ end
 
 
 """
-    embed(basis1[, basis2], indices::Vector, operators::Vector)
+    embed(basis1[, basis2], indices::Vector, op::AbstractOperator)
 
 Embed operator acting on a joint Hilbert space where missing indices are filled up with identity operators.
 """
