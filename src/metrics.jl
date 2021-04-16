3f529a3019bd4d8f85e5f3be1003d60e41136053
KKleinbeck <kekleinbeck@gmail.com>
2021-04-16 14:43:40 +0200

    Fix documentation of `negativity` (#19)

--- src/metrics.jl
+++ src/metrics.jl
@@ -227,7 +227,7 @@ Negativity of rho with respect to subsystem index.
 The negativity of a density matrix ρ is defined as
 
 ```math
-N(ρ) = \\|ρᵀ\\|,
+N(ρ) = \\frac{\\|ρᵀ\\|-1}{2},
 ```
 where `ρᵀ` is the partial transpose.
 """
