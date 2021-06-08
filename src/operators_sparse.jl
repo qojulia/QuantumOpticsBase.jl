78978bbebf443d07cb02bf4e3d8badd5ca035a14
David Plankensteiner <david.plankensteiner@gmx.at>
2021-06-08 09:21:56 +0200

    Typed dense and sparse operators

--- src/operators_sparse.jl
+++ src/operators_sparse.jl
@@ -19,8 +19,10 @@ SparseOperator(b1::Basis, b2::Basis, data::SparseMatrixCSC) = Operator(b1, b2, d
 SparseOperator(b::Basis, data) = SparseOperator(b, b, data)
 SparseOperator(op::DataOperator) = SparseOperator(op.basis_l, op.basis_r, op.data)
 
-SparseOperator(b1::Basis, b2::Basis) = SparseOperator(b1, b2, spzeros(ComplexF64, length(b1), length(b2)))
-SparseOperator(b::Basis) = SparseOperator(b, b)
+SparseOperator(::Type{T},b1::Basis,b2::Basis) where T = SparseOperator(b1,b2,spzeros(T,length(b1),length(b2)))
+SparseOperator(::Type{T},b::Basis) where T = SparseOperator(b,b,spzeros(T,length(b),length(b)))
+SparseOperator(b1::Basis, b2::Basis) = SparseOperator(ComplexF64, b1, b2)
+SparseOperator(b::Basis) = SparseOperator(ComplexF64, b, b)
 
 """
     sparse(op::AbstractOperator)
