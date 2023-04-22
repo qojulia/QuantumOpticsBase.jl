e315bc9523658b5983a62c2910fd2ccf95a85fd4
Ashley Milsted <ammilste@amazon.com>
2023-04-21 21:52:22 -0700

    Only turn off ambiguity checking for reshape

--- test/test_aqua.jl
+++ test/test_aqua.jl
@@ -3,8 +3,8 @@ using QuantumOpticsBase
 using Aqua
 
 @testset "aqua" begin
-    Aqua.test_all(QuantumOpticsBase,
+    Aqua.test_all(QuantumOpticsBase;
                   piracy=false,  # TODO: Due to Base methods in QuantumOpticsBase, for types defined in QuantumInterface
-                  ambiguities=false,  # FIXME: TEMPORARILY WORK AROUND AMBIGUITIES IN FILLARRAYS
+                  ambiguities=(exclude=[Base.reshape],)  # FIXME: Temporarily work-around ambiguities from FillArrays
                   )
 end # testset
