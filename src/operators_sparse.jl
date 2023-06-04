f92106ac3ace739de0787582c788f7c7bea0f4ff
Stefan Krastanov <stefan@krastanov.org>
2023-06-04 01:13:56 -0400

    document the purposeful piracy of identityoperator

--- src/operators_sparse.jl
+++ src/operators_sparse.jl
@@ -68,8 +68,7 @@ identityoperator(::Type{T}, ::Type{S}, b1::Basis, b2::Basis) where {T<:DataOpera
 identityoperator(::Type{T}, ::Type{S}, b::Basis) where {T<:DataOperator,S<:Number} =
     Operator(b, b, Eye{S}(length(b)))
 
-identityoperator(::Type{T}, b1::Basis, b2::Basis) where T<:Number = identityoperator(DataOperator, T, b1, b2) # TODO this is piracy over QuantumInterface, that hardcodes the use of QuantumOpticsBase.identityoperator
-identityoperator(::Type{T}, b::Basis) where T<:Number = identityoperator(DataOperator, T, b) # TODO this is piracy over QuantumInterface, that hardcodes the use of QuantumOpticsBase.identityoperator
+identityoperator(::Type{T}, b1::Basis, b2::Basis) where T<:Number = identityoperator(DataOperator, T, b1, b2) # XXX This is purposeful type piracy over QuantumInterface, that hardcodes the use of QuantumOpticsBase.DataOperator in identityoperator. Also necessary for backward compatibility.
 
 """
     diagonaloperator(b::Basis)
--- test/test_aqua.jl
+++ test/test_aqua.jl
@@ -5,6 +5,10 @@ using FillArrays
 
 @testset "aqua" begin
     Aqua.test_all(QuantumOpticsBase;
-                  ambiguities=(exclude=[FillArrays.reshape],)  # Due to https://github.com/JuliaArrays/FillArrays.jl/issues/105#issuecomment-1518406018
+                  ambiguities=(exclude=[FillArrays.reshape],),  # Due to https://github.com/JuliaArrays/FillArrays.jl/issues/105#issuecomment-1518406018
+                  piracy=(broken=true,)
                   )
+    # manual piracy check to exclude identityoperator
+    pirates = [pirate for pirate in Aqua.Piracy.hunt(QuantumOpticsBase) if pirate.name != :identityoperator]
+    @test isempty(pirates)
 end # testset
