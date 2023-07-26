066d3765571d9525a8c1a2fa6094b0a9b7f46eee
Ashley Milsted <ashmilsted@gmail.com>
2023-07-26 13:47:03 -0700

    Update docstring

--- src/time_dependent_operator.jl
+++ src/time_dependent_operator.jl
@@ -7,7 +7,7 @@ import Base: size, *, +, -, /, ==, isequal, adjoint, convert
 Abstract type providing a time-dependent operator interface. Time-dependent
 operators have internal "clocks" that can be addressed with [`set_time!`](@ref)
 and [`current_time`](@ref). A shorthand `op(t)`, equivalent to
-`set_time!(op, t)`, is available for brevity.
+`set_time!(copy(op), t)`, is available for brevity.
 
 A time-dependent operator is always concrete-valued according to the current
 time of its internal clock.
