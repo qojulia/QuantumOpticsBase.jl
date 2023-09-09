859bb34411528055f116354a86804fb944f7458b
Abhishek Bhatt <abhishekbhatt730@gmail.com>
2023-09-09 10:24:15 -0400

    modify apply!

--- src/apply.jl
+++ src/apply.jl
@@ -1,5 +1,13 @@
 function apply!(state::Ket, indices, operation::Operator)
     op = basis(state)==basis(operation) ? operation : embed(basis(state), indices, operation)
+    if (length(indices)>1)
+        for i in 2:length(indices)
+            if indices[i]<indices[i-1]
+                op = permutesystems(op, indices)
+                break
+            end
+        end
+    end
     state.data = (op*state).data
     state
 end
