54d195a469c8e6f4f338b5be735b1bb6d74cd8c8
Abhishek Bhatt <abhishekbhatt730@gmail.com>
2023-09-13 11:22:37 -0400

    permutation for operator and tests

--- src/apply.jl
+++ src/apply.jl
@@ -14,6 +14,14 @@ end
 
 function apply!(state::Operator, indices, operation::Operator)
     op = basis(state)==basis(operation) ? operation : embed(basis(state), indices, operation)
+    if (length(indices)>1)
+        for i in 2:length(indices)
+            if indices[i]<indices[i-1]
+                op = permutesystems(op, indices)
+                break
+            end
+        end
+    end
     state.data = (op*state*op').data
     state
 end
