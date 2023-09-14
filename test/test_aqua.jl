b478f186952b910336c1a64c173ca643db0dda15
Akira Kyle <akira@akirakyle.com>
2023-09-13 18:53:14 -0600

    Disable recursive checking of method ambiguities in Aqua

--- test/test_aqua.jl
+++ test/test_aqua.jl
@@ -5,7 +5,7 @@ using FillArrays
 
 @testset "aqua" begin
     Aqua.test_all(QuantumOpticsBase;
-                  ambiguities=(exclude=[FillArrays.reshape],),  # Due to https://github.com/JuliaArrays/FillArrays.jl/issues/105#issuecomment-1518406018
+                  ambiguities=(recursive=false),
                   piracy=(broken=true,)
                   )
     # manual piracy check to exclude identityoperator
