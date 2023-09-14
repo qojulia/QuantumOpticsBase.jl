3c7e645a811ba992ca3380b1307834d5e61e3bd5
Stefan Krastanov <stefan@krastanov.org>
2023-09-13 20:26:15 -0400

    fixes for julia 1.6

--- src/apply.jl
+++ src/apply.jl
@@ -32,7 +32,7 @@ end
 
 function apply!(state::Operator, indices, operation::T) where {T<:AbstractSuperOperator}
     if length(indices)>1
-        throw("Applying SuperOperator to multiple qubits/operators is not supported currently, due to missing tensor product method for SuperOperators")
+        error("Applying SuperOperator to multiple qubits/operators is not supported currently, due to missing tensor product method for SuperOperators")
     end
     op = basis(state)==basis(operation) ? operation : embed(basis(state), indices, operation)
     state.data = (op*state).data
--- test/test_apply.jl
+++ test/test_apply.jl
@@ -3,7 +3,7 @@ using QuantumInterface
 using Test
 
 @testset "apply" begin
-    
+
 _b2 = SpinBasis(1//2)
 _l0 = spinup(_b2)
 _l1 = spindown(_b2)
@@ -52,11 +52,11 @@ catch e
     #@test typeof(e) <: QuantumInterface.IncompatibleBases
 end
 
-#test CNOT₂₋₁
+# test CNOT₂₋₁
 CNOT = DenseOperator(_b2⊗_b2, Complex.([1 0 0 0; 0 0 0 1; 0 0 1 0; 0 1 0 0]))
 @test QuantumOpticsBase.apply!(_l0⊗_l1, [2,1], CNOT) == _l1⊗_l1
 
-#test operator permutation with 3 qubits/operators for apply!
+# test operator permutation with 3 qubits/operators for apply!
 
 # 3-qubit operator permutation
 @test QuantumOpticsBase.apply!(_l0⊗_l1⊗_l0, [2,3,1], _x⊗_y⊗_z) ≈ (_y*_l0)⊗(_z*_l1)⊗(_x*_l0)
@@ -64,13 +64,13 @@ CNOT = DenseOperator(_b2⊗_b2, Complex.([1 0 0 0; 0 0 0 1; 0 0 1 0; 0 1 0 0]))
 # 3-operator operator permutation
 @test QuantumOpticsBase.apply!(_x⊗_y⊗_z, [2,3,1], _x⊗_y⊗_z) ≈ (_y⊗_z⊗_x)*(_x⊗_y⊗_z)*(_y⊗_z⊗_x)'
 
-#3-qubit/operator errors when called for applying superoperator
+# 3-qubit/operator errors when called for applying superoperator
 sOp1 = spre(create(FockBasis(1)))
 sOp2 = spost(create(FockBasis(1)))
 st1 = coherentstate(FockBasis(1), 1.4)
 st2 = coherentstate(FockBasis(1), 0.3)
 st3 = coherentstate(FockBasis(1), 0.8)
 
-@test_throws "Applying SuperOperator to multiple qubits/operators is not supported currently, due to missing tensor product method for SuperOperators" QuantumOpticsBase.apply!(st1⊗st2⊗st3, [2,3,1], sOp1)
+@test_throws ErrorException QuantumOpticsBase.apply!(st1⊗st2⊗st3, [2,3,1], sOp1)
 
 end #testset
