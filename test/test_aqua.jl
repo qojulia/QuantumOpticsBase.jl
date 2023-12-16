5ad71582770c217718848e14f649ee7b39b2b6db
Stefan Krastanov <stefan@krastanov.org>
2023-12-16 15:32:56 -0500

    CompatHelper updates and JET/Aqua cleanup (#150)
    Co-authored-by: CompatHelper Julia <compathelper_noreply@julialang.org>

--- test/test_aqua.jl
+++ test/test_aqua.jl
@@ -8,7 +8,7 @@ using StatsBase
     Aqua.test_all(QuantumOpticsBase;
                   ambiguities=(exclude=[FillArrays.reshape, # Due to https://github.com/JuliaArrays/FillArrays.jl/issues/105#issuecomment-1518406018
                                         StatsBase.TestStat, StatsBase.:(==) , StatsBase.sort!],),  # Due to https://github.com/JuliaStats/StatsBase.jl/issues/861
-                  piracy=(broken=true,)
+                  piracies=(broken=true,)
                   )
     # manual piracy check to exclude identityoperator
     pirates = [pirate for pirate in Aqua.Piracy.hunt(QuantumOpticsBase) if pirate.name ∉ [:identityoperator,:identitysuperoperator]]
