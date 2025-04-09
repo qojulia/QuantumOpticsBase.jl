52b7f29f2b9711f550c77ad1d2746f0efd485080
Akira Kyle <akira@akirakyle.com>
2025-04-08 23:09:09 -0600

    More Superoperator WIP

--- src/superoperators.jl
+++ src/superoperators.jl
@@ -1,9 +1,26 @@
 import QuantumInterface: KetBraBasis, ChoiBasis
 
+const SuperKetType = Ket{<:KetBraBasis}
+
+const DenseSuperOpPureType{BL,BR} = Operator{<:KetBraBasis,<:KetBraBasis,<:Matrix}
+const DenseSuperOpAdjType{BL,BR} = Operator{<:KetBraBasis,<:KetBraBasis,<:Adjoint{<:Number,<:Matrix}}
+const DenseSuperOpType{BL,BR} = Union{DenseOpPureType{<:KetBraBasis,<:KetBraBasis},DenseOpAdjType{<:KetBraBasis,BR}}
+const SparseSuperOpPureType{BL,BR} = Operator{<:KetBraBasis,<:KetBraBasis,<:SparseMatrixCSC}
+const SparseSuperOpAdjType{BL,BR} = Operator{<:KetBraBasis,<:KetBraBasis,<:Adjoint{<:Number,<:SparseMatrixCSC}}
+const SparseSuperOpType{BL,BR} = Union{SparseOpPureType{<:KetBraBasis,<:KetBraBasis},SparseOpAdjType{<:KetBraBasis,BR}}
+
 const SuperOperatorType = Operator{<:KetBraBasis,<:KetBraBasis}
-const ChoiStateType = Operator{CompositeBasis{<:Integer,<:ChoiBasis},CompositeBasis{<:Integer,<:ChoiBasis}}
+const ChoiStateType = Operator{<:ChoiBasis,<:ChoiBasis}
+
+#const ChoiBasisType = Union{CompositeBasis{ChoiBasis,T} where {T}, CompositeBasis{ChoiBasis{B},T} where {B,T}}
+#const ChoiStateType = Operator{ChoiBasisType,ChoiBasisType}
+#const ChoiStateType = Union{Operator{CompositeBasis{ChoiBasis,S},CompositeBasis{ChoiBasis,T}} where {S,T}, Operator{CompositeBasis{ChoiBasis{BL},S},CompositeBasis{ChoiBasis{BR},T}} where {BL,BR,S,T}}
 
 vec(op::Operator) = Ket(KetBraBasis(basis_l(op), basis_r(op)), reshape(op.data, length(op.data)))
+function unvec(k::SuperKetType)
+    bl, br = basis_l(basis(k)), basis_r(basis(k))
+    return Operator(bl, br, reshape(k.data, length(bl), length(br)))
+end
 
 function spre(op::Operator)
     multiplicable(op, op) || throw(ArgumentError("It's not clear what spre of a non-square operator should be. See issue #113"))
@@ -12,10 +29,10 @@ end
 
 function spost(op::Operator)
     multiplicable(op, op) || throw(ArgumentError("It's not clear what spost of a non-square operator should be. See issue #113"))
-    SuperOperator(KetBraBasis(basis_r(op)), (op.basis_l, op.basis_l), kron(permutedims(op.data), identityoperator(op).data))
+    Operator(KetBraBasis(basis_r(op)), KetBraBasis(basis_l(op)), kron(permutedims(op.data), identityoperator(op).data))
 end
 
-sprepost(A::Operator, B::Operator) = SuperOperator((A.basis_l, B.basis_r), (A.basis_r, B.basis_l), kron(permutedims(B.data), A.data))
+sprepost(A::Operator, B::Operator) = Operator(KetBraBasis(A.basis_l, B.basis_r), KetBraBasis(A.basis_r, B.basis_l), kron(permutedims(B.data), A.data))
 
 # reshape swaps within systems due to colum major ordering
 # https://docs.qojulia.org/quantumobjects/operators/#tensor_order
@@ -31,8 +48,7 @@ function choi(op::SuperOperatorType)
     bl, br = basis_l(op), basis_r(op)
     (l1, l2), (r1, r2) = (basis_l(bl), basis_r(bl)), (basis_l(br), basis_r(br))
     (l1, l2), (r1, r2), data = _super_choi((l1, l2), (r1, r2), op.data)
-    return Operator(ChoiBasis(l1,true)⊗ChoiBasis(l2,false),
-                    ChoiBasis(r1,true)⊗ChoiBasis(r2,false), data)
+    return Operator(ChoiBasis(l1,l2), ChoiBasis(r1,r2), data)
 end
 
 function super(op::ChoiStateType)
@@ -44,8 +60,17 @@ end
 
 dagger(a::ChoiStateType) = choi(dagger(super(a)))
 
-*(a::SuperOperatorType, b::Operator) = a*vec(b)
+*(a::SuperOperatorType, b::SuperOperatorType) = (check_multiplicable(a,b); Operator(a.basis_l, b.basis_r, a.data*b.data))
+*(a::SuperOperatorType, b::Operator) = unvec(a*vec(b))
 *(a::ChoiStateType, b::SuperOperatorType) = super(a)*b
 *(a::SuperOperatorType, b::ChoiStateType) = a*super(b)
 *(a::ChoiStateType, b::ChoiStateType) = choi(super(a)*super(b))
 *(a::ChoiStateType, b::Operator) = super(a)*b
+
+identitysuperoperator(b::Basis) = Operator(KetBraBasis(b,b), KetBraBasis(b,b), Eye{ComplexF64}(length(b)^2))
+
+identitysuperoperator(op::DenseSuperOpType) = 
+    Operator(op.basis_l, op.basis_r, Matrix(one(eltype(op.data))I, size(op.data)))
+
+identitysuperoperator(op::SparseSuperOpType) = 
+    Operator(op.basis_l, op.basis_r, sparse(one(eltype(op.data))I, size(op.data)))
--- test/test_superoperators.jl
+++ test/test_superoperators.jl
@@ -1,37 +1,8 @@
 using Test
 using QuantumOpticsBase
 using SparseArrays, LinearAlgebra
-import QuantumOpticsBase: ChoiState # Remove when ChoiState is publicly exported
 
 @testset "superoperators" begin
-
-# Test creation
-b = GenericBasis(3)
-@test_throws DimensionMismatch DenseSuperOperator((b, b), (b, b), zeros(ComplexF64, 3, 3))
-@test_throws DimensionMismatch SparseSuperOperator((b, b), (b, b), spzeros(ComplexF64, 3, 3))
-@test_throws DimensionMismatch ChoiState((b, b), (b, b), zeros(ComplexF64, 3, 3))
-
-# Test copy, sparse and dense
-b1 = GenericBasis(2)
-b2 = GenericBasis(7)
-b3 = GenericBasis(5)
-b4 = GenericBasis(3)
-
-s = DenseSuperOperator((b1, b2), (b3, b4))
-s_ = dense(s)
-s_.data[1,1] = 1
-@test s.data[1,1] == 0
-s_sparse = sparse(s_)
-@test isa(s_sparse, SparseSuperOpType)
-@test s_sparse.data[1,1] == 1
-
-s = SparseSuperOperator((b1, b2), (b3, b4))
-s_ = sparse(s)
-s_.data[1,1] = 1
-@test s.data[1,1] == 0
-s_dense = dense(s_)
-@test isa(s_dense, DenseSuperOpType)
-@test s_dense.data[1,1] == 1
 
 # Test length
 b1 = GenericBasis(3)
@@ -51,76 +22,76 @@ b2 = GenericBasis(5)
 b3 = GenericBasis(5)
 b4 = GenericBasis(3)
 
-d1 = DenseSuperOperator((b1, b2), (b3, b4))
-d2 = DenseSuperOperator((b3, b4), (b1, b2))
-s1 = SparseSuperOperator((b1, b2), (b3, b4))
-s2 = SparseSuperOperator((b3, b4), (b1, b2))
+d1 = DenseOperator(KetBraBasis(b1, b2), KetBraBasis(b3, b4))
+d2 = DenseOperator(KetBraBasis(b3, b4), KetBraBasis(b1, b2))
+s1 = SparseOperator(KetBraBasis(b1, b2), KetBraBasis(b3, b4))
+s2 = SparseOperator(KetBraBasis(b3, b4), KetBraBasis(b1, b2))
 
 x = d1*d2
 @test isa(x, DenseSuperOpType)
-@test x.basis_l == x.basis_r == (b1, b2)
+@test x.basis_l == x.basis_r == KetBraBasis(b1, b2)
 
 x = s1*s2
 @test isa(x, SparseSuperOpType)
-@test x.basis_l == x.basis_r == (b1, b2)
+@test x.basis_l == x.basis_r == KetBraBasis(b1, b2)
 
 x = d1*s2
 @test isa(x, DenseSuperOpType)
-@test x.basis_l == x.basis_r == (b1, b2)
+@test x.basis_l == x.basis_r == KetBraBasis(b1, b2)
 
 x = s1*d2
 @test isa(x, DenseSuperOpType)
-@test x.basis_l == x.basis_r == (b1, b2)
+@test x.basis_l == x.basis_r == KetBraBasis(b1, b2)
 
 x = d1*3
 @test isa(x, DenseSuperOpType)
-@test x.basis_l == (b1, b2)
-@test x.basis_r == (b3, b4)
+@test x.basis_l == KetBraBasis(b1, b2)
+@test x.basis_r == KetBraBasis(b3, b4)
 
 x = 2.5*s1
 @test isa(x, SparseSuperOpType)
-@test x.basis_l == (b1, b2)
-@test x.basis_r == (b3, b4)
+@test x.basis_l == KetBraBasis(b1, b2)
+@test x.basis_r == KetBraBasis(b3, b4)
 
 x = d1 + d1
 @test isa(x, DenseSuperOpType)
-@test x.basis_l == (b1, b2)
-@test x.basis_r == (b3, b4)
+@test x.basis_l == KetBraBasis(b1, b2)
+@test x.basis_r == KetBraBasis(b3, b4)
 
 x = s1 + s1
 @test isa(x, SparseSuperOpType)
-@test x.basis_l == (b1, b2)
-@test x.basis_r == (b3, b4)
+@test x.basis_l == KetBraBasis(b1, b2)
+@test x.basis_r == KetBraBasis(b3, b4)
 
 x = d1 + s1
 @test isa(x, DenseSuperOpType)
-@test x.basis_l == (b1, b2)
-@test x.basis_r == (b3, b4)
+@test x.basis_l == KetBraBasis(b1, b2)
+@test x.basis_r == KetBraBasis(b3, b4)
 
 x = d1 - d1
 @test isa(x, DenseSuperOpType)
-@test x.basis_l == (b1, b2)
-@test x.basis_r == (b3, b4)
+@test x.basis_l == KetBraBasis(b1, b2)
+@test x.basis_r == KetBraBasis(b3, b4)
 
 x = s1 - s1
 @test isa(x, SparseSuperOpType)
-@test x.basis_l == (b1, b2)
-@test x.basis_r == (b3, b4)
+@test x.basis_l == KetBraBasis(b1, b2)
+@test x.basis_r == KetBraBasis(b3, b4)
 
 x = d1 - s1
 @test isa(x, DenseSuperOpType)
-@test x.basis_l == (b1, b2)
-@test x.basis_r == (b3, b4)
+@test x.basis_l == KetBraBasis(b1, b2)
+@test x.basis_r == KetBraBasis(b3, b4)
 
 x = -d1
 @test isa(x, DenseSuperOpType)
-@test x.basis_l == (b1, b2)
-@test x.basis_r == (b3, b4)
+@test x.basis_l == KetBraBasis(b1, b2)
+@test x.basis_r == KetBraBasis(b3, b4)
 
 x = -s1
 @test isa(x, SparseSuperOpType)
-@test x.basis_l == (b1, b2)
-@test x.basis_r == (b3, b4)
+@test x.basis_l == KetBraBasis(b1, b2)
+@test x.basis_r == KetBraBasis(b3, b4)
 
 # TODO: Clean-up this part
 ωc = 1.2
@@ -130,7 +101,6 @@ g = 1.0
 κ = 1.1
 
 T = Float64[0.,1.]
-
 
 fockbasis = FockBasis(7)
 spinbasis = SpinBasis(1//2)
@@ -155,45 +125,45 @@ J = [Ja, Jc]
 ρ₀ = dm(Ψ₀)
 
 @test identitysuperoperator(spinbasis)*sx == sx
-@test ChoiState(identitysuperoperator(spinbasis))*sx == sx
+@test choi(identitysuperoperator(spinbasis))*sx == sx
 @test identitysuperoperator(sparse(spre(sx)))*sx == sparse(sx)
-@test ChoiState(identitysuperoperator(sparse(spre(sx))))*sx == sparse(sx)
-@test sparse(ChoiState(identitysuperoperator(spre(sx))))*sx == sparse(sx)
+@test choi(identitysuperoperator(sparse(spre(sx))))*sx == sparse(sx)
+@test sparse(choi(identitysuperoperator(spre(sx))))*sx == sparse(sx)
 @test identitysuperoperator(dense(spre(sx)))*sx == dense(sx)
-@test ChoiState(identitysuperoperator(dense(spre(sx))))*sx == dense(sx)
-@test dense(ChoiState(identitysuperoperator(spre(sx))))*sx == dense(sx)
+@test choi(identitysuperoperator(dense(spre(sx))))*sx == dense(sx)
+@test dense(choi(identitysuperoperator(spre(sx))))*sx == dense(sx)
 
 op1 = DenseOperator(spinbasis, [1.2+0.3im 0.7+1.2im;0.3+0.1im 0.8+3.2im])
 op2 = DenseOperator(spinbasis, [0.2+0.1im 0.1+2.3im; 0.8+4.0im 0.3+1.4im])
 @test tracedistance(spre(op1)*op2, op1*op2) < 1e-12
-@test tracedistance(ChoiState(spre(op1))*op2, op1*op2) < 1e-12
+@test tracedistance(choi(spre(op1))*op2, op1*op2) < 1e-12
 @test tracedistance(spost(op1)*op2, op2*op1) < 1e-12
-@test tracedistance(ChoiState(spost(op1))*op2, op2*op1) < 1e-12
+@test tracedistance(choi(spost(op1))*op2, op2*op1) < 1e-12
 
 @test spre(sparse(op1))*op2 == op1*op2
-@test ChoiState(spre(sparse(op1)))*op2 == op1*op2
+@test choi(spre(sparse(op1)))*op2 == op1*op2
 @test spost(sparse(op1))*op2 == op2*op1
-@test ChoiState(spost(sparse(op1)))*op2 == op2*op1
+@test choi(spost(sparse(op1)))*op2 == op2*op1
 @test spre(sparse(dagger(op1)))*op2 == dagger(op1)*op2
-@test ChoiState(spre(sparse(dagger(op1))))*op2 == dagger(op1)*op2
+@test choi(spre(sparse(dagger(op1))))*op2 == dagger(op1)*op2
 @test spre(dense(dagger(op1)))*op2 ≈ dagger(op1)*op2
-@test ChoiState(spre(dense(dagger(op1))))*op2 ≈ dagger(op1)*op2
+@test choi(spre(dense(dagger(op1))))*op2 ≈ dagger(op1)*op2
 @test sprepost(sparse(op1), op1)*op2 ≈ op1*op2*op1
-@test ChoiState(sprepost(sparse(op1), op1))*op2 ≈ op1*op2*op1
+@test choi(sprepost(sparse(op1), op1))*op2 ≈ op1*op2*op1
 
 @test spre(sparse(op1))*sparse(op2) == sparse(op1*op2)
-@test ChoiState(spre(sparse(op1)))*sparse(op2) == sparse(op1*op2)
+@test choi(spre(sparse(op1)))*sparse(op2) == sparse(op1*op2)
 @test spost(sparse(op1))*sparse(op2) == sparse(op2*op1)
-@test ChoiState(spost(sparse(op1)))*sparse(op2) == sparse(op2*op1)
+@test choi(spost(sparse(op1)))*sparse(op2) == sparse(op2*op1)
 @test sprepost(sparse(op1), sparse(op1))*sparse(op2) ≈ sparse(op1*op2*op1)
-@test ChoiState(sprepost(sparse(op1), sparse(op1)))*sparse(op2) ≈ sparse(op1*op2*op1)
+@test choi(sprepost(sparse(op1), sparse(op1)))*sparse(op2) ≈ sparse(op1*op2*op1)
 
 @test sprepost(op1, op2) ≈ spre(op1)*spost(op2)
 b1 = FockBasis(1)
 b2 = FockBasis(5)
 op = fockstate(b1, 0) ⊗ dagger(fockstate(b2, 0))
 @test sprepost(dagger(op), op)*dm(fockstate(b1, 0)) == dm(fockstate(b2, 0))
-@test ChoiState(sprepost(dagger(op), op))*dm(fockstate(b1, 0)) == dm(fockstate(b2, 0))
+@test choi(sprepost(dagger(op), op))*dm(fockstate(b1, 0)) == dm(fockstate(b2, 0))
 @test_throws ArgumentError spre(op)
 @test_throws ArgumentError spost(op)
 
@@ -203,17 +173,17 @@ for j=J
     ρ .+= j*ρ₀*dagger(j) - 0.5*dagger(j)*j*ρ₀ - 0.5*ρ₀*dagger(j)*j
 end
 @test tracedistance(L*ρ₀, ρ) < 1e-10
-@test tracedistance(ChoiState(L)*ρ₀, ρ) < 1e-10
+@test tracedistance(choi(L)*ρ₀, ρ) < 1e-10
 
 # tout, ρt = timeevolution.master([0.,1.], ρ₀, H, J; reltol=1e-7)
 # @test tracedistance(exp(dense(L))*ρ₀, ρt[end]) < 1e-6
 # @test tracedistance(exp(sparse(L))*ρ₀, ρt[end]) < 1e-6
 
 @test dense(spre(op1)) == spre(op1)
-@test dense(ChoiState(spre(op1))) == ChoiState(spre(op1))
+@test dense(choi(spre(op1))) == choi(spre(op1))
 
 @test L/2.0 == 0.5*L == L*0.5
-@test -L == SparseSuperOperator(L.basis_l, L.basis_r, -L.data)
+@test -L == SparseOperator(L.basis_l, L.basis_r, -L.data)
 
 @test_throws AssertionError liouvillian(H, J; rates=zeros(4, 4))
 
@@ -256,12 +226,12 @@ o_l = fockstate(b_fock, 2)
 encoder_kraus = z_l ⊗ dagger(spinup(b_logical)) + o_l ⊗ dagger(spindown(b_logical))
     encoder_sup = sprepost(encoder_kraus, dagger(encoder_kraus))
 decoder_sup = sprepost(dagger(encoder_kraus), encoder_kraus)
-@test SuperOperator(ChoiState(encoder_sup)).data == encoder_sup.data
+@test super(choi(encoder_sup)).data == encoder_sup.data
 @test decoder_sup == dagger(encoder_sup)
-@test ChoiState(decoder_sup) == dagger(ChoiState(encoder_sup))
+@test choi(decoder_sup) == dagger(choi(encoder_sup))
 @test decoder_sup*encoder_sup ≈ dense(identitysuperoperator(b_logical))
-@test decoder_sup*ChoiState(encoder_sup) ≈ dense(identitysuperoperator(b_logical))
-@test ChoiState(decoder_sup)*encoder_sup ≈ dense(identitysuperoperator(b_logical))
-@test SuperOperator(ChoiState(decoder_sup)*ChoiState(encoder_sup)) ≈ dense(identitysuperoperator(b_logical))
+@test decoder_sup*choi(encoder_sup) ≈ dense(identitysuperoperator(b_logical))
+@test choi(decoder_sup)*encoder_sup ≈ dense(identitysuperoperator(b_logical))
+@test super(choi(decoder_sup)*choi(encoder_sup)) ≈ dense(identitysuperoperator(b_logical))
 
 end # testset
