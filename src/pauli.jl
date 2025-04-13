45b047b6107deb84a25ddff8b079e791d97fcffb
Akira Kyle <akira@akirakyle.com>
2025-04-12 21:21:49 -0600

    More WIP pauli

--- src/pauli.jl
+++ src/pauli.jl
@@ -1,70 +1,74 @@
 using TransmuteDims
 
-const PauliTransferType = Operator{<:PauliBasis,<:PauliBasis}
+#function _pauli_to_ketbra1()
+#    b = SpinBasis(1//2)
+#    vec_it(fn) = vec(fn(b).data)
+#    p2kb = hcat(map(vec_it, [identityoperator, sigmax, sigmay, sigmaz])...)
+#end
+#
+#function _ketbra_to_pauli(N)
+#    T = kron((_pauli_to_ketbra1 for _=1:N)...)
+#    T = reshape(T, Tuple(4 for _=1:2N))
+#    T = PermutedDimsArray(T, ((2i-1 for i=1:N)..., (2i for i=1:N)...))
+#    reshape(T, (4^N, 4^N))
+#end
+#
+#function _op_to_pauli(Nl, Nr, data)
+#    Ul = ket_bra_to_pauli(Nl)
+#    Ur = Nl == Nr ? Ul : _ketbra_to_pauli(Nr)
+#    data = dagger(Ul) * data * Ur # TODO figure out normalization
+#    @assert isapprox(imag.(data), zero(data), atol=tol)
+#    Operator(PauliBasis()^Nl, PauliBasis()^Nr, real.(data))
+#end
 
-# TODO should either of these functions be cached?
-function _pauli_to_ketbra1()
+function _pauli_to_comp1()
     b = SpinBasis(1//2)
     vec_it(fn) = vec(fn(b).data)
-    p2kb = hcat(map(vec_it, [identityoperator, sigmax, sigmay, sigmaz])...)
+    V = hcat(map(vec_it, [identityoperator, sigmax, sigmay, sigmaz])...)
+    Operator(PauliBasis(), KetBraBasis(b, b), V)
 end
 
-function _ketbra_to_pauli(N)
-    T = kron((_pauli_to_ketbra1 for _=1:N)...)
-    T = reshape(T, Tuple(4 for _=1:2N))
-    T = PermutedDimsArray(T, ((2i-1 for i=1:N)..., (2i for i=1:N)...))
-    reshape(T, (4^N, 4^N))
-end
+_pauli_to_comp1_cached = _pauli_to_comp1()
 
-function _op_to_pauli(Nl, Nr, data)
-    Ul = ket_bra_to_pauli(Nl)
-    Ur = Nl == Nr ? Ul : _ketbra_to_pauli(Nr)
-    data = dagger(Ul) * data * Ur # TODO figure out normalization
-    @assert isapprox(imag.(data), zero(data), atol=tol)
-    Operator(PauliBasis()^Nl, PauliBasis()^Nr, real.(data))
-end
-
+# TODO should this be further cached?
 """
     comp_to_pauli(N)
 
 Creates a superoperator which changes from the computational `SpinBasis(1//2)`
 to the `PauliBasis()` over `N` qubits.
 """
-function comp_to_pauli(N)
-    T = kron((_pauli_to_ketbra1 for _=1:N)...)
-    T = reshape(T, Tuple(4 for _=1:2N))
-    T = PermutedDimsArray(T, ((2i-1 for i=1:N)..., (2i for i=1:N)...))
-    sb = SpinBasis(1//2)^N
-    Operator(PauliBasis()^N, KetBraBasis(sb, sb), reshape(T, (4^N, 4^N))
+function pauli_to_comp(N::Integer)
+    N > 0 || throw(ArgumentError())
+    N == 1 && return _pauli_to_comp1_cached
+    V = pauli_to_comp(N÷2)
+    (N%2 == 0) && return V⊗V
+    return V⊗V⊗pauli_to_comp(1)
 end
 
-function pauli(op::SuperOperatorType; tol=1e-9)
-    bl, br = basis_l(op), basis_r(op)
-    ((basis_l(bl) == basis_r(bl)) && (basis_l(br) == basis_r(br))) || throw(ArgumentError("Superoperator must map between square operators in order to be converted to pauli represenation"))
-
-    for b in (basis_l(bl), basis_l(br))
-        for i=1:length(b)
-            (b[i] isa SpinBasis && dimension(b[i]) == 2) || throw(ArgumentError("Superoperator must be over systems composed of SpinBasis(1//2) to be converted to pauli representation"))
-        end
+function _check_is_spinbasis(b)
+    for i=1:length(b)
+        (b[i] isa SpinBasis && dimension(b[i]) == 2) || throw(ArgumentError("Superoperator must be over systems composed of SpinBasis(1//2) to be converted to pauli representation"))
     end
+end
+
+function pauli(op::SOpKetBraType)
+    ((basis_l(basis_l(op)) == basis_r(basis_l(op))) && (basis_l(br) == basis_r(basis_r(op)))) || throw(ArgumentError("Superoperator must map between square operators in order to be converted to pauli represenation"))
+    bl, br = basis_l(basis_l(op)), basis_l(basis_r(op))
+    foreach(_check_is_spinbasis, (bl, br))
 
     Nl, Nr = length(basis_l(bl)), length(basis_l(br))
-    Ul = ket_bra_to_pauli(Nl)
-    Ur = Nl == Nr ? Ul : _ketbra_to_pauli(Nr)
-    data = dagger(Ul)*op.data*Ur # TODO figure out normalization
-    @assert isapprox(imag.(data), zero(data), atol=tol)
-    Operator(PauliBasis()^Nl, PauliBasis()^Nr, real.(data))
+    Vl = pauli_to_comp(Nl)
+    Vr = Nl == Nr ? Vl : pauli_to_comp(Nr)
+    #data = dagger(Ul)*op.data*Ur # TODO figure out normalization
+    #@assert isapprox(imag.(data), zero(data), atol=tol)
+    #Operator(PauliBasis()^Nl, PauliBasis()^Nr, real.(data))
+    dagger(Vl) * op * Vr # TODO figure out normalization
 end
 
-function chi(op::ChoiStateType; tol=1e-9)
+function chi(op::ChoiStateType)
     (basis_l(op) == basis_r(op)) || throw(ArgumentError("Choi state must map between square operators in order to be converted to chi represenation"))
-
     bl, br = basis_l(basis_l(op)), basis_r(basis_l(op))
-    for b in (bl, br)
-        for i=1:length(b)
-            (b[i] isa SpinBasis && dimension(b[i]) == 2) || throw(ArgumentError("Choi state must be over systems composed of SpinBasis(1//2) to be converted to chi representation"))
-        end
-    end
+    foreach(_check_is_spinbasis, (bl, br))
 
     Nl, Nr = length(bl), length(br)
     Ul = ket_bra_to_pauli(Nl)
--- src/superoperators.jl
+++ src/superoperators.jl
@@ -1,32 +1,31 @@
-import QuantumInterface: KetBraBasis, ChoiBasis
+import QuantumInterface: KetBraBasis, ChoiBasis, PauliBasis
 using TensorCast
 
-const SuperKetType = Ket{<:KetBraBasis}
+const SOpBasis = Union{KetBraBasis, PauliBasis}
+const SuperOperatorType{BL,BR,T} = Operator{BL,BR,T} where {BL<:SOpBasis,BR<:SOpBasis}
+const SOpKetBraType{BL,BR,T} = Operator{BL,BR,T} where {BL<:KetBraBasis,BR<:KetBraBasis}
+const SOpPauliType{BL,BR,T} = Operator{BL,BR,T} where {BL<:PauliBasis,BR<:PauliBasis}
+const ChoiStateType{BL,BR,T} = Operator{BL,BR,T} where {BR<:ChoiBasis,BL<:ChoiBasis}
+const ChoiKetBraType{BL,BR,T} = Operator{ChoiBasis{BL,BR},ChoiBasis{BL,BR},T} where {BL<:KetBraBasis,BR<:KetBraBasis}
+const ChiType{BL,BR,T} = Operator{ChoiBasis{BL,BR},ChoiBasis{BL,BR},T} where {BL<:PauliBasis,BR<:PauliBasis}
 
-const DenseSuperOpPureType{BL,BR} = Operator{<:KetBraBasis,<:KetBraBasis,<:Matrix}
-const DenseSuperOpAdjType{BL,BR} = Operator{<:KetBraBasis,<:KetBraBasis,<:Adjoint{<:Number,<:Matrix}}
-const DenseSuperOpType{BL,BR} = Union{DenseOpPureType{<:KetBraBasis,<:KetBraBasis},DenseOpAdjType{<:KetBraBasis,BR}}
-const SparseSuperOpPureType{BL,BR} = Operator{<:KetBraBasis,<:KetBraBasis,<:SparseMatrixCSC}
-const SparseSuperOpAdjType{BL,BR} = Operator{<:KetBraBasis,<:KetBraBasis,<:Adjoint{<:Number,<:SparseMatrixCSC}}
-const SparseSuperOpType{BL,BR} = Union{SparseOpPureType{<:KetBraBasis,<:KetBraBasis},SparseOpAdjType{<:KetBraBasis,BR}}
-
-const SuperOperatorType = Operator{<:KetBraBasis,<:KetBraBasis}
-const ChoiStateType = Operator{<:ChoiBasis,<:ChoiBasis}
-
-#const ChoiBasisType = Union{CompositeBasis{ChoiBasis,T} where {T}, CompositeBasis{ChoiBasis{B},T} where {B,T}}
-#const ChoiStateType = Operator{ChoiBasisType,ChoiBasisType}
-#const ChoiStateType = Union{Operator{CompositeBasis{ChoiBasis,S},CompositeBasis{ChoiBasis,T}} where {S,T}, Operator{CompositeBasis{ChoiBasis{BL},S},CompositeBasis{ChoiBasis{BR},T}} where {BL,BR,S,T}}
+const DenseSuperOpPureType{BL,BR} = SuperOperatorType{BL,BR,<:Matrix}
+const DenseSuperOpAdjType{BL,BR} = SuperOperatorType{BL,BR,<:Adjoint{<:Number,<:Matrix}}
+const DenseSuperOpType{BL,BR} = Union{DenseOpPureType{BL,BL},DenseOpAdjType{BL,BR}}
+const SparseSuperOpPureType{BL,BR} = SuperOperatorType{BL,BR,<:SparseMatrixCSC}
+const SparseSuperOpAdjType{BL,BR} = SuperOperatorType{BL,BR,<:Adjoint{<:Number,<:SparseMatrixCSC}}
+const SparseSuperOpType{BL,BR} = Union{SparseOpPureType{BL,BR},SparseOpAdjType{BL,BR}}
 
 vec(op::Operator) = Ket(KetBraBasis(basis_l(op), basis_r(op)), vec(op.data))
-function unvec(k::SuperKetType)
-    bl, br = basis_l(basis(k)), basis_r(basis(k))
-    Operator(bl, br, reshape(k.data, dimension(bl), dimension(br)))
-end
-#function unvec(k::SuperKetType)
+#function unvec(k::Ket{<:KetBraBasis})
 #    bl, br = basis_l(basis(k)), basis_r(basis(k))
-#    @cast A[n,m] |= k.data[(n,m)] (n ∈ 1:dimension(bl), m ∈ 1:dimension(br))
-#    return Operator(bl, br, A)
+#    Operator(bl, br, reshape(k.data, dimension(bl), dimension(br)))
 #end
+function unvec(k::Ket{<:KetBraBasis})
+    bl, br = basis_l(basis(k)), basis_r(basis(k))
+    @cast A[n,m] |= k.data[(n,m)] (n ∈ 1:dimension(bl), m ∈ 1:dimension(br))
+    return Operator(bl, br, A)
+end
 
 function spre(op::Operator)
     multiplicable(op, op) || throw(ArgumentError("It's not clear what spre of a non-square operator should be. See issue #113"))
@@ -37,40 +36,66 @@ function spost(op::Operator)
     multiplicable(op, op) || throw(ArgumentError("It's not clear what spost of a non-square operator should be. See issue #113"))
     sprepost(identityoperator(op), op)
 end
-#function sprepost(A::Operator, B::Operator)
-#    @cast C[(ν,μ), (n,m)] |= A.data[ν,n] * B.data[m,μ]
-#    Operator(KetBraBasis(basis_l(A), basis_r(B)), KetBraBasis(basis_r(A), basis_l(B)), C)
-#end
 
 #sprepost(A::Operator, B::Operator) = Operator(KetBraBasis(basis_l(A), basis_r(B)), KetBraBasis(basis_r(A), basis_l(B)), kron(permutedims(B.data), A.data))
-
 function sprepost(A::Operator, B::Operator)
     @cast C[(ν,μ), (n,m)] |= A.data[ν,n] * B.data[m,μ]
     Operator(KetBraBasis(basis_l(A), basis_r(B)), KetBraBasis(basis_r(A), basis_l(B)), C)
 end
 
-function _super_choi((l1, l2), (r1, r2), data)
-    data = Base.ReshapedArray(data, map(length, (l2, l1, r2, r1)), ())
-    (l1, l2), (r1, r2) = (r2, l2), (r1, l1)
-    data = PermutedDimsArray(data, (1, 3, 2, 4))
-    data = Base.ReshapedArray(data, map(length, (l1⊗l2, r1⊗r2)), ())
-    return (l1, l2), (r1, r2), copy(data)
+function tensor(A::SuperOperatorType, B::SuperOperatorType)
+    a1, a2 = basis_l(A), basis_r(A)
+    b1, b2 = basis_l(B), basis_r(B)
+    da1, da2, db1, db2 = map(dimension, (a1, a2, b1, b2))
+    @cast A[(ν,μ), (n,m)] |= A[m,μ] * B[n,ν] (m ∈ 1:da1, μ ∈ 1:da2, n ∈ 1:db1, ν ∈ 1:db2)
+    return Operator(a1⊗b2, a2⊗b2, A)
 end
 
-# Sec IV.A. of  https://arxiv.org/abs/1111.6950
+#function _sch(data, l1, l2, r1, r2)
+#    data = reshape(data, map(dimension, (l1, l2, r1, r2)))
+#    data = permutedims(data, (4, 2, 3, 1))
+#    reshape(data, map(dimension, (l2⊗r2, l1⊗r1)))
+#end
+#
+#function _sch(data::SparseMatrixCSC, l1, l2, r1, r2)
+#    #data = reshape(sparse(data), map(dimension, (l1, l2, r1, r2)), ())
+#    #data = _permutedims(sparse(data), size(data), (4, 2, 3, 1))
+#    data = _permutedims(sparse(data), map(dimension, (l1, l2, r1, r2)), (4, 2, 3, 1))
+#    sparse(reshape(data, map(dimension, (l2⊗r2, l1⊗r1))))
+#    # sparse(::ReshapedArray) only works when ndims == 2
+#end
+
+#sprepost(A::Operator, B::Operator) = Operator(KetBraBasis(basis_l(A), basis_r(B)), KetBraBasis(basis_r(A), basis_l(B)), kron(permutedims(B.data), A.data))
+
+#function _super_choi((l1, l2), (r1, r2), data)
+#    data = Base.ReshapedArray(data, map(length, (l2, l1, r2, r1)), ())
+#    (l1, l2), (r1, r2) = (r2, l2), (r1, l1)
+#    data = PermutedDimsArray(data, (1, 3, 2, 4))
+#    data = Base.ReshapedArray(data, map(length, (l1⊗l2, r1⊗r2)), ())
+#    return (l1, l2), (r1, r2), copy(data)
+#end
+
+# Sec IV.A. of https://arxiv.org/abs/1111.6950
 function _super_choi(basis_fn, op)
     l1, l2 = basis_l(basis_l(op)), basis_r(basis_l(op))
     r1, r2 = basis_l(basis_r(op)), basis_r(basis_r(op))
-    d1, d2, d3, d4 = map(dimension, (l1, l2, r1, r2))
-    @cast A[(ν,μ), (n,m)] |= op.data[(m,μ), (n,ν)] (m ∈ 1:d1, μ ∈ 1:d2, n ∈ 1:d3, ν ∈ 1:d4)
+#    data = _sch(op.data, l1, l2, r1, r2)
+#    data = reshape(op.data, map(dimension, (l1, l2, r1, r2)))
+#    data = permutedims(data, (4, 2, 3, 1))
+#    data = reshape(data, map(dimension, (l2⊗r2, l1⊗r1)))
+    dl1, dl2, dr1, dr2 = map(dimension, (l1, l2, r1, r2))
+    @cast A[(ν,μ), (n,m)] |= op.data[(m,μ), (n,ν)] (m ∈ 1:dl1, μ ∈ 1:dl2, n ∈ 1:dr1, ν ∈ 1:dr2)
     return Operator(basis_fn(r2, l2), basis_fn(r1, l1), A)
 end
 
 choi(op::SuperOperatorType) = _super_choi(ChoiBasis, op)
 super(op::ChoiStateType) =  _super_choi(KetBraBasis, op)
 
+# I'm not sure this is actually right... see sec V.C. of https://arxiv.org/abs/1111.6950
 dagger(a::ChoiStateType) = choi(dagger(super(a)))
 
+# This method is necessary so we don't fall back to the method below it
+*(a::SuperOperatorType, b::SuperOperatorType) = (check_multiplicable(a,b); Operator(a.basis_l, b.basis_r, a.data*b.data))
 *(a::SuperOperatorType, b::Operator) = unvec(a*vec(b))
 *(a::ChoiStateType, b::SuperOperatorType) = super(a)*b
 *(a::SuperOperatorType, b::ChoiStateType) = a*super(b)
