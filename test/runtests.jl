abf906e68fe1625c85361ad9c9946f2356a08aeb
Stefan Krastanov <github.acc@krastanov.org>
2025-06-04 14:02:16 -0400

    run only jet test if ENV is set

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,18 +3,18 @@ using QuantumOpticsBase
 
 # filter for the test
 testfilter = ti -> begin
-  exclude = Symbol[]
   if get(ENV,"JET_TEST","")!="true"
-    push!(exclude, :jet)
+    return :jet in ti.tags
   end
+
+  exclude = Symbol[]
   if !(VERSION >= v"1.10")
-    push!(exclude, :doctests)
     push!(exclude, :aqua)
+    push!(exclude, :doctests)
   end
 
   return all(!in(exclude), ti.tags)
 end
-
 println("Starting tests with $(Threads.nthreads()) threads out of `Sys.CPU_THREADS = $(Sys.CPU_THREADS)`...")
 
 @run_package_tests filter=testfilter
