fb193a2a3be560c9b44a68bc0b83c5f47e65be92
Aniket-umass <aniketmishra@umass.edu>
2026-08-05 10:54:06 -0400

    Match Fock conventions: use WeakDepHelpers import; drop unused OrdinaryDiffEq test dep

--- src/QuantumOpticsBase.jl
+++ src/QuantumOpticsBase.jl
@@ -3,7 +3,7 @@ module QuantumOpticsBase
 using SparseArrays, LinearAlgebra, LRUCache, Strided, UnsafeArrays, FillArrays
 import LinearAlgebra: mul!, rmul!
 import RecursiveArrayTools
-import WeakDepHelpers: WeakDepCache, @declare_method_is_in_extension, register_weakdep_cache
+using WeakDepHelpers: WeakDepCache, register_weakdep_cache, @declare_method_is_in_extension
 
 import QuantumInterface: dagger, directsum, ⊕, dm, embed, nsubsystems, expect, identityoperator, identitysuperoperator,
         permutesystems, projector, ptrace, reduced, tensor, ⊗, variance, apply!, basis, AbstractSuperOperator
